d84d899927f67f11cf4a17320260d83a60559f3a
Stuti Anand <stutianand@example.com>
2025-07-23 00:40:45 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HelloBankingApp
+
+A simple Spring Boot app for Celebal Internship Project.
---
+++ src/main/java/com/celebal/hellobanking/HelloController.java
@@ -0,0 +1,13 @@
+package com.celebal.hellobanking; // use your actual package
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Spring Boot!";
+    }
+}
